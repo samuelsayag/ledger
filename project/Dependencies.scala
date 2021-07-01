8f7691923dce26067d8c8bf09e54b89aa06d82c0
Samuel SAYAG <samuel.sayag@clicktale.com>
2021-07-01 07:51:34 +0300

    init

---
+++ project/Dependencies.scala
@@ -0,0 +1,17 @@
+import sbt._
+
+object Dependencies {
+  val slickVersion = "3.3.3"
+  val zioVersion = "1.0.9"
+  lazy val zio = "dev.zio" %% "zio" % zioVersion
+  lazy val `zio-prelude` = "dev.zio" %% "zio-prelude" % "1.0.0-RC5"
+  lazy val `zio-test` = "dev.zio" %% "zio-test" % zioVersion % Test
+  lazy val `zio-test-sbt` = "dev.zio" %% "zio-test-sbt" % zioVersion % Test
+  lazy val `zio-test-magnolia` =
+    "dev.zio" %% "zio-test-magnolia" % zioVersion % Test // optional
+  lazy val slick = "com.typesafe.slick" %% "slick" % slickVersion
+  lazy val `slick-hikaricp` =
+    "com.typesafe.slick" %% "slick-hikaricp" % slickVersion
+  lazy val postgres = "org.postgresql" % "postgresql" % "42.2.22"
+  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+}
